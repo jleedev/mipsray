b89780deeee37cc64d0a53817cf41e27b5fb9f64
Josh Lee <jleedev@gmail.com>
2008-04-03 21:49:32 +0000

    Camera rays closer to being finished...
    git-svn-id: http://squire.case.edu/svn/mipsray@13 23de7761-3f18-4d56-8861-33b65c0cb227

--- main.s
+++ main.s
@@ -6,6 +6,8 @@ filename:
 main:
 	# Stack usage: 52 bytes
 	# 0($sp) camera ray (48 bytes)
+	# - 0($sp) location vector (0,8,16)
+	# - 24($sp) direction vector (24,32,40)
 	# 48($sp) return address (4 bytes)
 	addi $sp, $sp, -52
 	sw $ra, 48($sp)
@@ -27,6 +29,15 @@ main:
 
 	jal bmp.write_header
 
+	# The camera ray's position never changes, so load it here.
+	la $t0, location
+	l.d $f0, 0($t0)
+	s.d $f0, 0($sp)
+	l.d $f0, 8($t0)
+	s.d $f0, 8($sp)
+	l.d $f0, 16($t0)
+	s.d $f0, 16($sp)
+
 	# For each pixel in the image:
 	li $s3, 0 # y coord of current pixel
 main.loop1:
@@ -34,19 +45,30 @@ main.loop1:
 
 main.loop2:
 
-	# Compute the ray from the camera to that pixel
-
-	# the ray's position is the camera's position
-	# the ray's direction is as follows
-	# (x-w/2)/w * right + (y-h/2)/h * up + direction
-	# = (x/w-1/2) * right + (y/h-1/2) * up + direction
+	# Compute the camera ray
+	# the ray's position was loaded above
+	# the ray's direction is
+	# a * right + b * up + direction
+	# a = (x/w-1/2), b = (y/h-1/2)
 	mtc1 $s0, $f30 # width
 	cvt.d.w $f30, $f30
 	mtc1 $s2, $f28 # x
-	div.d $f30, $f30, $f28
+	div.d $f30, $f28, $f30
 	lui $t0, 0x3fc0 # 0.5
 	mtc1 $t0, $f29
-	mul.d $f30, $f28
+	mtc1 $0, $f28
+	sub.d $f30, $f30, $f28 # $f30 = a
+	# load right into <f0,f2,f4> and call scalar.v
+	la $t0, right
+	l.d $f0, 0($t0)
+	l.d $f2, 8($t0)
+	l.d $f4, 16($t0)
+	jal scalar.v
+	# store this part temporarily
+	s.d $f0, 32($sp)
+	s.d $f2, 40($sp)
+	s.d $f4, 48($sp)
+	# TODO ...
 
 	# Trace that ray to get a color
 	# Output that pixel to the output file
@@ -61,6 +83,7 @@ main.loop2:
 
 	addi $s3, $s3, 1 # move to the next row
 	bne $s3, $s1 main.loop1
+	# End for
 
 	# Close the file and exit
 	move $a0, $s4
